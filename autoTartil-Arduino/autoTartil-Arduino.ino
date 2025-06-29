d25af416b2a262b75b094ac81126559ffbf7859a
Irfan0209 <irfanardiansyah0209@gmail.com>
2025-06-29 17:48:25 +0700

    merubah logika program pada auto tartil

--- autoTartil-Arduino/autoTartil-Arduino.ino
+++ autoTartil-Arduino/autoTartil-Arduino.ino
@@ -5,7 +5,7 @@
   - Arduino UNO: membaca konfigurasi via Serial, menyimpan konfigurasi dalam array
   - Arduino mengatur jadwal berdasarkan waktu dan memutar rekaman tartil dan adzan
   - DFPlayer Mini: memainkan file tartil & adzan
-  - Relay: mengaktifkan power amplifier saat audio diputar, dimatikan setelah delay
+  - Relay: mengaktifkan power amplifier saat audio diputar
 */
 
 #include <SoftwareSerial.h>
@@ -13,11 +13,11 @@
 #include <EEPROM.h>
 #include <TimeLib.h>
 
-SoftwareSerial dfSerial(2, 3);
+SoftwareSerial dfSerial(2, 3); // RX, TX ke DFPlayer
 DFRobotDFPlayerMini dfplayer;
 
-#define RELAY_PIN 7
-#define HARI_TOTAL 8
+#define RELAY_PIN 13
+#define HARI_TOTAL 8 // 7 hari + SemuaHari (index ke-7)
 #define WAKTU_TOTAL 5
 #define MAX_FILE 11
 #define MAX_FOLDER 10
@@ -64,22 +64,28 @@ bool manualSedangDiputar = false;
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW);
+  digitalWrite(RELAY_PIN, LOW); // Awal mati
 
   Serial.begin(9600);
   dfSerial.begin(9600);
+ // loadFromEEPROM();
+  durasiTartil[0][0] = 10; // Folder 1, File 1 = 10 detik
+  durasiTartil[0][1] = 12;
+  durasiTartil[0][2] = 8;
+  durasiAdzan[0]     = 15;
 
   if (!dfplayer.begin(dfSerial)) {
     Serial.println("DFPlayer tidak terdeteksi!");
     while (1);
   }
-
   dfplayer.volume(volumeDFPlayer);
   Serial.println("Sistem Auto Tartil Siap.");
 }
 
 void loop() {
-  if (sudahEksekusi && millis() - lastTriggerMillis > 60000) sudahEksekusi = false;
+  if (sudahEksekusi && millis() - lastTriggerMillis > 60000) {
+    sudahEksekusi = false;
+  }
   bacaDataSerial();
   cekDanPutarSholatNonBlocking();
   cekSelesaiTartil();
@@ -111,6 +117,48 @@ void parseData(String data) {
       currentDay = hari;
     }
     return;
+  }
+  if (data.startsWith("VOL:")) {
+    volumeDFPlayer = data.substring(4).toInt();
+    dfplayer.volume(volumeDFPlayer);
+    saveToEEPROM();
+    return;
+  }
+
+  if (data.startsWith("HR:")) {
+    int idxHR = data.indexOf("HR:");
+    int idxW = data.indexOf("|W:");
+    if (idxHR == -1 || idxW == -1) return;
+
+    int hari = data.substring(idxHR + 3, idxW).toInt();
+    String segmen = data.substring(idxW + 3);
+
+    int koma = 0;
+    byte waktuIdx = getIntPart(segmen, koma);
+    if (waktuIdx >= WAKTU_TOTAL) return;
+
+    WaktuConfig &cfg = jadwal[hari][waktuIdx];
+    cfg.aktif       = getIntPart(segmen, koma);
+    cfg.aktifAdzan  = getIntPart(segmen, koma);
+    cfg.fileAdzan   = getIntPart(segmen, koma);
+    cfg.tartilDulu  = getIntPart(segmen, koma);
+    cfg.durasiTartil= getIntPart(segmen, koma);
+    cfg.folder      = getIntPart(segmen, koma);
+
+    String listStr = segmen.substring(koma);
+    for (int i = 0; i < 3; i++) {
+      int dash = listStr.indexOf('-');
+      if (dash == -1 && listStr.length() > 0) {
+        cfg.list[i] = listStr.toInt();
+        break;
+      } else if (dash != -1) {
+        cfg.list[i] = listStr.substring(0, dash).toInt();
+        listStr = listStr.substring(dash + 1);
+      } else {
+        cfg.list[i] = 0;
+      }
+    }
+    saveToEEPROM();
   }
   if (data.startsWith("PLAY:")) {
     int idx = 5;
@@ -173,7 +221,7 @@ void cekDanPutarSholatNonBlocking() {
       digitalWrite(RELAY_PIN, HIGH);
       if (cfg.tartilDulu && totalDurasiTartil > 0) {
         tartilCount = 0;
-        for (byte i = 0; i < 3; i++) {
+        for (byte i = 0; i < 3; i++) { //rubah jumblah daftar list yang akan diputar
           if (cfg.list[i] > 0) tartilList[tartilCount++] = cfg.list[i];
         }
         if (tartilCount > 0) {
@@ -184,9 +232,10 @@ void cekDanPutarSholatNonBlocking() {
           tartilMulaiMillis = millis();
           tartilDurasi = getDurasiTartil(tartilFolder, tartilList[tartilIndex]) * 1000;
           tartilSedangDiputar = true;
+          Serial.println("tartilCount:" + String(tartilCount));
         }
       } else if (cfg.aktifAdzan) {
-        dfplayer.play(cfg.fileAdzan);
+        dfplayer.playFolder(11,cfg.fileAdzan);
         adzanMulaiMillis = millis();
         adzanDurasi = getDurasiAdzan(cfg.fileAdzan) * 1000;
         adzanSedangDiputar = true;
@@ -211,26 +260,90 @@ void cekSelesaiTartil() {
     } else {
       tartilSedangDiputar = false;
       if (currentCfg && currentCfg->aktifAdzan) {
+        dfplayer.playFolder(11,currentCfg->fileAdzan);
+        adzanMulaiMillis = millis();
+        adzanDurasi = getDurasiAdzan(currentCfg->fileAdzan) * 1000;
+        adzanSedangDiputar = true;
+      }
+//      } else {
+//        relayOffDelayMillis = millis();
+//        relayMenungguMati = true;
+//      }
+    }
+  }
+}
+/*
+void cekDanPutarSholatNonBlocking() {
+  int hariIdx = currentDay;
+
+  for (int w = 0; w < WAKTU_TOTAL; w++) {
+    WaktuConfig &cfg = jadwal[hariIdx][w];
+    if (!cfg.aktif || tartilSedangDiputar || adzanSedangDiputar) continue;
+
+    uint16_t totalDurasiTartil = 0;
+    for (int i = 0; i < 3; i++) {
+      if (cfg.list[i] > 0) {
+        totalDurasiTartil += getDurasiTartil(cfg.folder, cfg.list[i]);
+      }
+    }
+
+    int nowDetik = hour() * 3600 + minute() * 60 + second();
+    int targetDetik = jamSholat[w] * 3600 + menitSholat[w] * 60 - totalDurasiTartil;
+
+    if (nowDetik == targetDetik && !sudahEksekusi) {
+      digitalWrite(RELAY_PIN, HIGH); // Aktifkan relay
+
+      if (cfg.tartilDulu && totalDurasiTartil > 0) {
+        tartilCount = 0;
+        for (byte i = 0; i < 3; i++) {
+          if (cfg.list[i] > 0) {
+            tartilList[tartilCount++] = cfg.list[i];
+          }
+        }
+        if (tartilCount > 0) {
+          tartilIndex = 0;
+          tartilFolder = cfg.folder;
+          currentCfg = &cfg;
+          dfplayer.playFolder(tartilFolder, tartilList[tartilIndex]);
+          tartilMulaiMillis = millis();
+          tartilDurasi = getDurasiTartil(tartilFolder, tartilList[tartilIndex]) * 1000;
+          tartilSedangDiputar = true;
+        }
+      } else if (cfg.aktifAdzan) {
+        dfplayer.play(cfg.fileAdzan);
+        adzanMulaiMillis = millis();
+        adzanDurasi = getDurasiAdzan(cfg.fileAdzan) * 1000;
+        adzanSedangDiputar = true;
+      } else {
+        digitalWrite(RELAY_PIN, LOW); // Tidak ada audio, matikan relay
+      }
+      lastTriggerMillis = millis();
+      sudahEksekusi = true;
+    }
+  }
+}
+
+void cekSelesaiTartil() {
+  if (!tartilSedangDiputar) return;
+  if (millis() - tartilMulaiMillis >= tartilDurasi) {
+    tartilIndex++;
+    if (tartilIndex < tartilCount) {
+      dfplayer.playFolder(tartilFolder, tartilList[tartilIndex]);
+      tartilMulaiMillis = millis();
+      tartilDurasi = getDurasiTartil(tartilFolder, tartilList[tartilIndex]) * 1000;
+    } else {
+      tartilSedangDiputar = false;
+      if (currentCfg && currentCfg->aktifAdzan) {
         dfplayer.play(currentCfg->fileAdzan);
         adzanMulaiMillis = millis();
         adzanDurasi = getDurasiAdzan(currentCfg->fileAdzan) * 1000;
         adzanSedangDiputar = true;
       } else {
-        relayOffDelayMillis = millis();
-        relayMenungguMati = true;
+        digitalWrite(RELAY_PIN, LOW); // Tidak ada adzan, matikan relay
       }
     }
   }
-}
-
-void cekSelesaiAdzan() {
-  if (!adzanSedangDiputar) return;
-  if (millis() - adzanMulaiMillis >= adzanDurasi) {
-    adzanSedangDiputar = false;
-    relayOffDelayMillis = millis();
-    relayMenungguMati = true;
-  }
-}
+}*/
 
 void cekRelayOffDelay() {
   if (relayMenungguMati && millis() - relayOffDelayMillis >= 3000) {
@@ -238,4 +351,27 @@ void cekRelayOffDelay() {
     relayMenungguMati = false;
     manualSedangDiputar = false;
   }
+}
+void cekSelesaiAdzan() {
+  if (!adzanSedangDiputar) return;
+  if (millis() - adzanMulaiMillis >= adzanDurasi) {
+    adzanSedangDiputar = false;
+    digitalWrite(RELAY_PIN, LOW); // Matikan relay setelah adzan selesai
+  }
+}
+
+void loadFromEEPROM() {
+  int addr = 0;
+  EEPROM.get(addr, jadwal); addr += sizeof(jadwal);
+  EEPROM.get(addr, durasiAdzan); addr += sizeof(durasiAdzan);
+  EEPROM.get(addr, durasiTartil); addr += sizeof(durasiTartil);
+  EEPROM.get(addr, volumeDFPlayer); addr += sizeof(volumeDFPlayer);
+}
+
+void saveToEEPROM() {
+  int addr = 0;
+  EEPROM.put(addr, jadwal); addr += sizeof(jadwal);
+  EEPROM.put(addr, durasiAdzan); addr += sizeof(durasiAdzan);
+  EEPROM.put(addr, durasiTartil); addr += sizeof(durasiTartil);
+  EEPROM.put(addr, volumeDFPlayer); addr += sizeof(volumeDFPlayer);
 }
