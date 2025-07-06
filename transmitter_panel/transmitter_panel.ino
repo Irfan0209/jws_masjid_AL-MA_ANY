23f236da6ce12e7d08cb345df4b9c5d1e9104890
Irfan0209 <irfanardiansyah0209@gmail.com>
2025-07-06 23:44:56 +0700

    refactor

---
+++ transmitter_panel/transmitter_panel.ino
@@ -0,0 +1,269 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+#include <ESP_EEPROM.h>
+
+#define EEPROM_SIZE 512
+#define ADDR_MODE        0
+#define ADDR_PASSWORD    2
+
+char ssid[20]     = "JAM_PANEL";
+char password[20] = "00000000";
+
+ESP8266WebServer server(80);
+WebSocketsServer webSocket(81);
+
+IPAddress local_IP(192, 168, 2, 1);
+IPAddress gateway(192, 168, 2, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+bool clientReady[5] = { false, false, false, false, false };
+
+unsigned long lastTimeSend = 0;
+const unsigned long intervalSendTime = 60000; // 1 menit
+
+void getData(String input) {
+  Serial.println(input);
+  // Di sini bisa tambahkan pengolahan data lebih lanjut
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch (type) {
+    case WStype_CONNECTED:
+      //Serial.printf("[WS] Client %u connected\n", num);
+      clientReady[num] = false;
+      break;
+
+    case WStype_DISCONNECTED:
+      //Serial.printf("[WS] Client %u disconnected\n", num);
+      clientReady[num] = false;
+      break;
+
+    case WStype_TEXT: {
+      String msg = String((char*)payload);
+
+      if (msg == "CLIENT_READY") {
+        clientReady[num] = true;
+      } else if(msg == "restart"){
+        getData(msg);
+        delay(500);
+        ESP.restart();
+      } else {
+        getData(msg);
+      }
+      break;
+    }
+  }
+}
+
+void handleSetTime() {
+  String data = "";
+   if (server.hasArg("Tm")) {
+    data = server.arg("Tm");
+    data = "Tm=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Settingan jam berhasil diupdate");
+  }
+  if (server.hasArg("text")) {
+    data = server.arg("text");
+    data = "text=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Settingan text berhasil diupdate");
+  }
+  if (server.hasArg("name")) {
+    data = server.arg("name");
+    data = "name=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Settingan nama berhasil diupdate");
+  }
+  if (server.hasArg("Br")) {
+    data  = server.arg("Br");
+    data = "Br=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Kecerahan berhasil diupdate");
+  }
+  if (server.hasArg("Spdt")) {
+    data = server.arg("Spdt"); // Atur kecepatan date
+    data = "Spdt=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Kecepatan kalender berhasil diupdate");
+  }
+  if (server.hasArg("Sptx1")) {
+    data = server.arg("Sptx1"); // Atur kecepatan text
+    data = "Sptx1=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Kecepatan info 1 berhasil diupdate");
+  }
+  if (server.hasArg("Sptx2")) {
+    data = server.arg("Sptx2"); // Atur kecepatan text
+    data = "Sptx2=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Kecepatan info 2 berhasil diupdate");
+  }
+  if (server.hasArg("Spnm")) {
+    data = server.arg("Spnm"); // Atur kecepatan text
+    data = "Spnm=" + data;
+   // Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Kecepatan nama berhasil diupdate");
+  }
+  if (server.hasArg("Iq")) {
+    data = server.arg("Iq"); // Atur koreksi iqomah
+    data = "Iq=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"iqomah diupdate");
+  }
+  if (server.hasArg("Dy")) {
+    data = server.arg("Dy"); // Atur durasi adzan
+    data = "Dy=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"displayBlink diupdate");
+  }
+  if (server.hasArg("Kr")) {
+    data = server.arg("Kr"); // Atur koreksi waktu jadwal sholat
+    data = "Kr=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"Selisih jadwal sholat diupdate");
+  }
+  if (server.hasArg("Lt")) {
+    data = server.arg("Lt"); // Atur latitude
+    data = "Lt=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"latitude diupdate");
+  }
+  if (server.hasArg("Lo")) {
+    data = server.arg("Lo"); // Atur latitude
+    data = "Lo=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"longitude diupdate");
+  }
+  if (server.hasArg("Tz")) {
+    data = server.arg("Tz"); // Atur latitude
+    data = "Tz=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"timezone diupdate");
+  }
+  if (server.hasArg("Al")) {
+    data = server.arg("Al"); // Atur latitude
+    data = "Al=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"altitude diupdate");
+  }
+  if (server.hasArg("Da")) { 
+    data = server.arg("Da"); 
+    data = "Da=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");// "durasi adzan diupdate");
+  }
+  if (server.hasArg("CoHi")) {
+    data = server.arg("CoHi"); // Atur latitude
+    data = "CoHi=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain", "OK");//"coreksi hijriah diupdate");
+  }
+
+  if (server.hasArg("Bzr")) {
+    data = server.arg("Bzr"); // Atur status buzzer
+    data = "Bzr=" + data;
+    //Serial.println(data);
+    getData(data);
+    server.send(200, "text/plain","OK");// (stateBuzzer) ? "Suara Diaktifkan" : "Suara Dimatikan");
+  }
+  if (server.hasArg("mode")) {
+    data = server.arg("mode"); // Atur status buzzer
+    data = "mode=" + data;
+    kirimDataKeClient();
+    getData(data);
+    server.send(200, "text/plain","OK");// (stateBuzzer) ? "Suara Diaktifkan" : "Suara Dimatikan");
+  }
+  if (server.hasArg("status")) {
+    server.send(200, "text/plain", "CONNECTED");
+  }
+ 
+  if (server.hasArg("newPassword")) {
+      data = server.arg("newPassword");
+      data = "newPassword=" + data;
+      //Serial.println(data);
+      getData(data);
+      server.send(200, "text/plain","OK");// "Password WiFi diupdate");
+    } 
+  data="";
+  //EEPROM.commit();
+}
+
+void AP_init() {
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAP(ssid, password);
+  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+
+  delay(1000);
+//  Serial.print("AP IP address: ");
+//  Serial.println(WiFi.softAPIP());
+
+  server.on("/setPanel", handleSetTime);
+  server.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+  //Serial.println("Web & WebSocket Server started");
+}
+
+void kirimDataKeClient() {
+  String data = "mode=1";
+  for (uint8_t i = 0; i < 5; i++) {
+    if (clientReady[i] && webSocket.clientIsConnected(i)) {
+      webSocket.sendTXT(i, data);
+    } else {
+      clientReady[i] = false;
+    }
+  }
+}
+
+void cekSerialMonitor() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+    //Serial.print("[Serial] Kirim ke semua client: ");
+    Serial.println(input);
+
+    for (uint8_t i = 0; i < 5; i++) {
+      if (clientReady[i] && webSocket.clientIsConnected(i)) {
+        webSocket.sendTXT(i, input);
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  EEPROM.begin(EEPROM_SIZE);
+  AP_init();
+}
+
+void loop() {
+  server.handleClient();
+  webSocket.loop();
+  cekSerialMonitor();
+
+//  if (millis() - lastTimeSend >= intervalSendTime) {
+//    lastTimeSend = millis();
+//    kirimDataKeClient();
+//  }
+}
